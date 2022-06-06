f5934243baf3725a2ecacf7a3f881fb3a36cddb6
Mohamed Nufail <eg173254@engug.ruh.ac.lk>
2022-06-06 20:56:37 +0530

    Added: Student model class with attributes

---
+++ Models/Students.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LMSystem.Models
+{
+    public class Student
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        [Required]
+        public string RegNo { get; set; }
+
+        [Required]
+        public int Batch { get; set; }
+    }
+}
