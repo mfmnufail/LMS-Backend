6fb91fe050a210513214b2d33f69ff2fc29f65ae
Mohamed Nufail <eg173254@engug.ruh.ac.lk>
2022-06-08 15:19:38 +0530

    Added : DbContxt class by introducing Students table

---
+++ Repository/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LMSystem.Models;
+
+namespace LMSystem.Repository
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+       
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Student>()
+                .HasIndex(e => e.RegNo)
+                .IsUnique();
+
+        }
+
+    }
+}
