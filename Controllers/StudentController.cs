8f0745f25c492a9a32ace74cef29841fb684bbc4
Mohamed Nufail <eg173254@engug.ruh.ac.lk>
2022-06-09 23:06:51 +0530

    Configured: Http Get List method using mediatr query

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LMSystem.Application.Students;
 using LMSystem.Models;
+using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LMSystem.Controllers
@@ -11,14 +14,23 @@ namespace LMSystem.Controllers
     [ApiController]
     public class StudentController : ControllerBase
     {
-      
+        private readonly IMediator _mediator;
+
+        public StudentController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
 
         // GET: api/Student
         [HttpGet]
         public async Task<ActionResult<List<Student>>> List()
         {
-            return null;
+            return await _mediator.Send(new List.Query());
         }
         
+
+
+
     }
 }
