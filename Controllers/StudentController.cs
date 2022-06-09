ce780bec38badb0b24cba3d43fb9c5a28ecb0d0f
Mohamed Nufail <eg173254@engug.ruh.ac.lk>
2022-06-09 23:00:39 +0530

    Added: Student Controller to handle Http requests and responses

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LMSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMSystem.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+      
+
+        // GET: api/Student
+        [HttpGet]
+        public async Task<ActionResult<List<Student>>> List()
+        {
+            return null;
+        }
+        
+    }
+}
