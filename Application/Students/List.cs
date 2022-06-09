c726a5d722b26e58a6555e63240d17da733a7d4e
Mohamed Nufail <eg173254@engug.ruh.ac.lk>
2022-06-09 23:04:41 +0530

    Added: MediatR to handle command query according to CQRS

---
+++ Application/Students/List.cs
@@ -0,0 +1,36 @@
+﻿
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using LMSystem.Models;
+using LMSystem.Repository;
+
+namespace LMSystem.Application.Students
+{
+    public class List
+    {
+
+        public class Query : IRequest<List<Student>> { }
+
+        public class Handler : IRequestHandler<Query, List<Student>>
+        {
+            private readonly AppDbContext _context;
+
+            public Handler(AppDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<List<Student>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var students = await _context.Students.ToListAsync();
+
+                return students;
+            }
+        }
+    }
+}
